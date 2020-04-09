5561a57a3a3cbff582fcb7c2bc7437064fdff243
schrotbe2021 <schrotbe2021@mountunion.edu>
2020-04-09 03:36:28 -0400

    JS for next and previous button for embla.

---
+++ src/js/prevAndNextButtons.js
@@ -0,0 +1,5 @@
+export const setUpPrevAndNextBtns = (prevButton, nextButton, embla) => {
+    prevButton.addEventListener('click', embla.scrollPrev, false)
+    nextButton.addEventListener('click', embla.scrollNext, false)
+    console.log("test")
+};
