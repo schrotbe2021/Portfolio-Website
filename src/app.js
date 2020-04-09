a8361a8b67c0fb518b8db8c077ffd47b2e20870f
schrotbe2021 <schrotbe2021@mountunion.edu>
2020-04-09 03:40:29 -0400

    Removed files.

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import EmblaCarousel from 'embla-carousel'
-
-const emblaNode = document.querySelector('.embla')
-const options = { loop: true,
-    startIndex: 0    
-}
-const embla = EmblaCarousel(emblaNode, options)
