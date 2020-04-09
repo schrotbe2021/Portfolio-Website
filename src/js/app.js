c2a38883896b2ed323df350517c5e99a3aefe8b5
schrotbe2021 <schrotbe2021@mountunion.edu>
2020-04-09 03:38:31 -0400

    Added styling and buttons for embla.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 import EmblaCarousel from 'embla-carousel'
+import { setUpPrevAndNextBtns } from './prevAndNextButtons'
 
 const emblaNode = document.querySelector('.embla')
-const options = { loop: true,
-    startIndex: 0    
+const options = { 
+    loop: true,
+    speed: 7
 }
+const prevButton = document.querySelector(".prevButton")
+const nextButton = document.querySelector(".nextButton")
 const embla = EmblaCarousel(emblaNode, options)
+
+setUpPrevAndNextBtns(prevButton, nextButton, embla);
